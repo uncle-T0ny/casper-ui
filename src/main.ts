87b2778847c319696a0fd1248a422c74def4fa83
faramo_zayw <faramo_zayw@protonmail.com>
2022-02-23 15:14:35 +0200

    added polyfill for `Buffer`

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import { Buffer } from "buffer";
+
+(window as any).Buffer = Buffer;
+
 const app = createApp(App)
 
 app.use(createPinia())
