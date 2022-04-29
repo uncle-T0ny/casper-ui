2c50c828541f0abc617c2d10ddac7935a1977d4d
A. Romankov <doit.rant@gmail.com>
2022-04-29 15:47:32 +0200

    get dict item

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,7 +1,113 @@
-import {CasperServiceByJsonRPC} from "casper-js-sdk";
+import {CasperServiceByJsonRPC, CLValueParsers, CLByteArray, CLString} from "casper-js-sdk";
+import {utils} from "casper-js-client-helper";
+import {concat} from "@ethersproject/bytes";
+import * as blake from "blakejs";
 export const NODE_ADDRESS = "http://65.108.9.249:7777/rpc";
 
-const casperService = new CasperServiceByJsonRPC(NODE_ADDRESS);
+const client = new CasperServiceByJsonRPC(NODE_ADDRESS);
+const BRIDGE_CONTRACT_HASH = '007c0c86b8a8b7bc19eb288875c203421d24f365972d8220a2fb855971166b8d';
+const LOCK_DEPLOY_HASH = 'b8dc91fec7e625b913b545cf7208c9a75b86846ac4c4916c44063c64d66a4eeb';
 
-casperService.getStateRootHash();
-console.log(casperService);
+type StoredLock = {
+    sender: String,
+    recipient: String,
+    destination: String,
+    amount: String,
+    token_source: String,
+    token_source_address: String,
+}
+class API {
+  async getStateRootHash() {
+    const { block } = await client.getLatestBlockInfo();
+    if (block) {
+      return block.header.state_root_hash;
+    } else {
+      throw Error("Problem when calling getLatestBlockInfo");
+    }
+  }
+
+  async getDeployInfo() {
+    const client = new CasperServiceByJsonRPC(NODE_ADDRESS);
+
+    return await client.getDeployInfo(LOCK_DEPLOY_HASH);
+  }
+
+  async getLockId(): Promise<string> {
+    const deployInfo = await this.getDeployInfo();
+    // @ts-ignore
+    const deployArgs = deployInfo.deploy?.session?.StoredContractByHash?.args;
+    console.log('deployArgs:', deployArgs)
+    // @ts-ignore
+    const lockIdEl = deployArgs.find(param => param[0] === 'lock_id');
+    console.log('lock id:', lockIdEl)
+    console.log(JSON.stringify(lockIdEl[1].parsed, null, 2));
+    return lockIdEl[1].parsed;
+  }
+
+  async getContractData(
+    nodeAddress: string,
+    stateRootHash: string,
+    contractHash: string,
+    path: string[] = []
+  ) {
+    const client = new CasperServiceByJsonRPC(nodeAddress);
+    const blockState = await client.getBlockState(
+      stateRootHash,
+      `hash-${contractHash}`,
+      path
+    );
+    return blockState;
+  }
+
+  async getContractDataByHash(
+    erc20ContractHash: string
+  ): Promise<{ namedKeys: object; contractPackageHash: string }> {
+    const stateRootHash = await this.getStateRootHash();
+
+    const contractData = await this.getContractData(
+      NODE_ADDRESS,
+      stateRootHash,
+      erc20ContractHash
+    );
+
+    const { contractPackageHash, namedKeys } = contractData.Contract!;
+    const namedKeysParsed = namedKeys.reduce((acc, val) => {
+      return { ...acc, [this.camelCased(val.name)]: val.key };
+    }, {});
+    return { namedKeys: namedKeysParsed, contractPackageHash };
+  }
+
+  private camelCased(myString: string) {
+    return myString.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
+  }
+
+  async run() {
+    const lockId = await this.getLockId();
+    const { namedKeys } = await this.getContractDataByHash(BRIDGE_CONTRACT_HASH);
+    console.log('namedKeys', namedKeys);
+    // @ts-ignore
+    const locksURef = namedKeys!.locks;
+    const itemKey = Buffer.from(lockId).toString("base64");
+    const finalBytes = concat([
+      CLValueParsers.toBytes(new CLString(lockId)).unwrap()
+    ]);
+
+    const blaked = blake.blake2b(finalBytes, undefined, 32);
+    const encodedBytes = Buffer.from(blaked).toString("hex");
+
+    console.log('encodedBytes', encodedBytes);
+
+    const storedValue = await client.getDictionaryItemByURef(
+      await this.getStateRootHash(),
+      lockId,
+      locksURef
+    );
+
+    console.log('lock result:', storedValue)
+  }
+}
+
+(async () => {
+  const api = new API();
+  await api.run();
+})();
