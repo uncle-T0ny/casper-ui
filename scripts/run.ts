3322db8f35d9362a5be32e29e3a7d38f9065487c
A. Romankov <doit.rant@gmail.com>
2022-05-03 12:55:42 +0200

    added getBalanceByPubKey

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,4 +1,12 @@
-import {CasperServiceByJsonRPC, CLValueParsers, CLByteArray, CLString} from "casper-js-sdk";
+import {
+  CasperServiceByJsonRPC,
+  CLValueParsers,
+  CLByteArray,
+  CLString,
+  CLKey,
+  CLAccountHash,
+  CLPublicKey
+} from "casper-js-sdk";
 import {utils} from "casper-js-client-helper";
 import {concat} from "@ethersproject/bytes";
 import * as blake from "blakejs";
@@ -7,6 +15,9 @@ export const NODE_ADDRESS = "http://65.108.9.249:7777/rpc";
 const client = new CasperServiceByJsonRPC(NODE_ADDRESS);
 const BRIDGE_CONTRACT_HASH = '007c0c86b8a8b7bc19eb288875c203421d24f365972d8220a2fb855971166b8d';
 const LOCK_DEPLOY_HASH = 'b8dc91fec7e625b913b545cf7208c9a75b86846ac4c4916c44063c64d66a4eeb';
+
+const ERC20_CONTRACT_HASH = 'f14592ab3940cd07e88b79c597f0cbb874ee22d8b833df292c8ceac1263ea86a';
+const ERC20_MINTER_PUB_KEY = '020294cd34c20d10f367dfcb9abdf7a800194e539c9dd40c46108e3f1c7613764eb4';
 
 type StoredLock = {
     sender: String,
@@ -109,9 +120,32 @@ class API {
     // @ts-ignore
     console.log('lock result:', Buffer.from(storedValue.CLValue.bytes, 'hex').toString('utf-8'))
   }
+
+  async getBalanceByPubKey() {
+    const { namedKeys } = await this.getContractDataByHash(ERC20_CONTRACT_HASH);
+
+
+    const keyBytes = CLValueParsers.toBytes(new CLKey(
+      new CLAccountHash((CLPublicKey.fromHex(ERC20_MINTER_PUB_KEY)).toAccountHash())
+    )).unwrap();
+    const itemKey = Buffer.from(keyBytes).toString("base64");
+
+    const storedValue = await client.getDictionaryItemByURef(
+      await this.getStateRootHash(),
+      itemKey,
+      //@ts-ignore
+      namedKeys!.balances,
+      { rawData : false }
+    );
+
+    console.log('storedValue', storedValue)
+  }
 }
+
+
 
 (async () => {
   const api = new API();
-  await api.run();
+  // await api.run();
+  await api.getBalanceByPubKey();
 })();
